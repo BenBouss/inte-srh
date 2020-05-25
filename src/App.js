c8df3b1c6872fa6363311286176056159ffef03a
Benoît Bousselot <benoit.bousselot.pro@gmail.com>
2020-05-25 13:06:31 +0200

    Router & 404 correction

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={ Homepage } />
+          <Route exact path="/inte-srh" component={ Homepage } />
           <Route path="/404" component={ PageNotFound } />
         </Switch>
       </Router>
