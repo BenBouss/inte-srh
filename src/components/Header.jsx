a16e59189a632fcfd578cfd8084cabd3f42dd549
Benoît Bousselot <benoit.bousselot.pro@gmail.com>
2020-06-02 23:36:59 +0200

    Add React-Tooltip & Back button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactTooltip from 'react-tooltip'
 import logoTissot from '../assets/logo-tissot.png'
 import MainNav from '../components/MainNav'
 import Searchbox from '../components/Searchbox'
@@ -6,12 +7,13 @@ import Searchbox from '../components/Searchbox'
 const Header = () => (
     <header className="mainHeader">
         <div className="sidebarHeader flex-items-center">
-            <div className="backButton">Mon Espace</div>
+            <div className="backButton" data-tip data-for="Retour"><a href="https://www.editions-tissot.fr/mon-espace/mon-compte.aspx"></a></div>
             <img src={logoTissot} alt="Edition Tissot" />
             <span className="appType">Documentation</span>
         </div>
         <MainNav />
         <Searchbox />
+        <ReactTooltip id="Retour" place="bottom" type="dark" effect="solid" arrowColor="transparent">Retour à Mon Espace</ReactTooltip>
     </header>
 );
 
